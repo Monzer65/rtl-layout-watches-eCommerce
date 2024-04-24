8a74b68c50c43b257688894e4a6243a3551e036a
monzer <danesh.monzer@gmail.com>
2024-04-24 19:04:38 +0330

    price range inputs completed

---
+++ app/store/products/filters/PriceRangeOptions.tsx
@@ -0,0 +1,147 @@
+"use client";
+import React, { useCallback, useEffect, useRef, useState } from "react";
+
+interface RangeInputProps {
+  min: number;
+  max: number;
+}
+
+const PriceRange: React.FC<RangeInputProps> = ({ min, max }) => {
+  const [range, setRange] = useState({ start: min, end: max });
+  const progressBarRef = useRef<HTMLDivElement>(null);
+  const startRangeRef = useRef<HTMLInputElement>(null);
+  const endRangeRef = useRef<HTMLInputElement>(null);
+  const startPriceRef = useRef<HTMLInputElement>(null);
+  const endPriceRef = useRef<HTMLInputElement>(null);
+
+  const updateProgressBar = useCallback(() => {
+    if (
+      progressBarRef.current &&
+      startRangeRef.current &&
+      endRangeRef.current
+    ) {
+      const startPercentage = (range.start / max) * 100;
+      progressBarRef.current.style.right = `${startPercentage}%`;
+      const widthPercentage = ((range.end - range.start) / (max - min)) * 100;
+      progressBarRef.current.style.width = `${widthPercentage}%`;
+    }
+  }, [max, min, range.end, range.start]);
+
+  useEffect(() => {
+    updateProgressBar();
+  }, [updateProgressBar]);
+
+  const handleStartChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const newStart = parseInt(event.target.value);
+    if (newStart > range.end) {
+      setRange((prevRange) => ({
+        ...prevRange,
+        end: newStart,
+        start: newStart,
+      }));
+    } else {
+      setRange((prevRange) => ({ ...prevRange, start: newStart }));
+    }
+    updateProgressBar();
+  };
+
+  const handleEndChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const newEnd = parseInt(event.target.value);
+    if (newEnd < range.start) {
+      setRange((prevRange) => ({ ...prevRange, start: newEnd, end: newEnd }));
+    } else {
+      setRange((prevRange) => ({ ...prevRange, end: newEnd }));
+    }
+    updateProgressBar();
+  };
+
+  const handleBlure = () => {
+    const startValue = Number(startPriceRef.current?.value);
+    const endValue = Number(endPriceRef.current?.value);
+    if (startValue > max) {
+      setRange((prev) => ({
+        ...prev,
+        start: max,
+        end: Math.min(endValue, max),
+      }));
+      return;
+    }
+
+    if (endValue > max) {
+      setRange((prev) => ({ ...prev, end: max }));
+      return;
+    }
+    setRange((prev) => ({ ...prev, start: startValue, end: endValue }));
+  };
+
+  return (
+    <div className='relative w-full pb-8 overflow-hidden'>
+      <div className='absolute w-full bottom-2 h-[10px] bg-gray-400 rounded-full'></div>
+      <div
+        ref={progressBarRef}
+        className='absolute bottom-2 left-0 h-[10px] bg-orange-300 rounded-full'
+      ></div>
+
+      <input
+        ref={startRangeRef}
+        type='range'
+        min={min}
+        max={max}
+        value={range.start}
+        onChange={handleStartChange}
+        className='w-full h-[10px] bg-none bg-transparent pointer-events-none rounded-s-full my-[10px] appearance-none focus:outline-none absolute -bottom-[2px] z-20 rounded-full'
+      />
+      <input
+        ref={endRangeRef}
+        type='range'
+        min={min}
+        max={max}
+        value={range.end}
+        onChange={handleEndChange}
+        className='w-full h-[10px] bg-none bg-transparent pointer-events-none rounded-e-full my-[10px] appearance-none focus:outline-none absolute -bottom-[2px] z-10 rounded-full'
+      />
+      <div className='flex flex-col gap-1 items-center'>
+        <div className='min-w-full flex items-center'>
+          از
+          <div className='relative border rounded-md font-bold text-lg mr-1 pr-1'>
+            <input
+              ref={startPriceRef}
+              type='number'
+              value={range.start}
+              onChange={handleStartChange}
+              onBlur={handleBlure}
+              min={min}
+              max={range.end}
+              placeholder=''
+              className='w-4/5 bg-none bg-transparent border-none outline-0 [appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none align-center'
+            />
+            <span className='absolute left-1 top-1/2 -translate-y-1/2 font-normal text-sm'>
+              تومان
+            </span>
+          </div>
+        </div>
+        <div className='min-w-full flex items-center'>
+          تا
+          <div className='relative border rounded-md font-bold text-lg mr-1 pr-1'>
+            <input
+              ref={endPriceRef}
+              type='number'
+              value={range.end}
+              onChange={handleEndChange}
+              onBlur={handleBlure}
+              min={range.start}
+              max={max}
+              placeholder=''
+              className='w-4/5 bg-none bg-transparent border-none outline-0 [appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none'
+            />
+            <span className='absolute left-1 top-1/2 -translate-y-1/2 font-normal text-sm'>
+              تومان
+            </span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PriceRange;
--- app/store/products/page.tsx
+++ app/store/products/page.tsx
@@ -1,5 +1,105 @@
+"use client";
+import { ChevronLeftIcon } from "@heroicons/react/24/outline";
+import { useState } from "react";
+import PriceRange from "./filters/PriceRangeOptions";
+
+const PriceRangeOptions = () => {
+  // Replace with your price range component
+  return <div>Price Range Component</div>;
+};
+
+const BrandOptions = () => {
+  // Replace with your brand checkboxes component
+  return <div>Brand Checkboxes Component</div>;
+};
+
+const ModelOptions = () => {
+  // Replace with your model options component
+  return <div>Model Options Component</div>;
+};
+
+const filters = [
+  { title: "بازه قیمت", children: <PriceRange min={0} max={10000} /> },
+  { title: "برند", children: <BrandOptions /> },
+  { title: "مدل", children: <ModelOptions /> },
+  { title: "اندازه قاب", children: "" },
+  { title: "شکل قاب", children: "" },
+  { title: "جنس بدنه", children: "" },
+  { title: "جنس بند", children: "" },
+  { title: "رنگ بدنه", children: "" },
+  { title: "رنگ صفحه", children: "" },
+  { title: "رنگ بند", children: "" },
+  { title: "نوع موتور", children: "" },
+  { title: "نیروی محرکه", children: "" },
+];
+
 const Products = () => {
-  return <main className='px-8'>Products</main>;
+  const [openFilters, setOpenFilters] = useState<{ [key: number]: boolean }>(
+    {}
+  );
+
+  const toggleFilter = (index: number) => {
+    setOpenFilters((prevState) => ({
+      ...prevState,
+      [index]: !prevState[index],
+    }));
+  };
+
+  return (
+    <main className='px-8 grid md:grid-cols-5 relative'>
+      <div className='min-h-[50px] max-h-[500px] md:sticky top-36 right-0 overflow-y-auto bg-red-400 p-2 [&>*:not(:last-child)]:border-b [&>*]:p-2 border border-gray-600 rounded-xl'>
+        <div className='flex justify-between border-none'>
+          <h3 className='text-xl font-semibold'>فیلترها</h3>
+          <button className='text-sm'>حذف همه فیلترها</button>
+        </div>
+        {filters.map((filter, i) => {
+          return (
+            <div key={i}>
+              <button
+                onClick={() => toggleFilter(i)}
+                className='w-full flex items-center justify-between'
+              >
+                <p>{filter.title}</p>
+                <ChevronLeftIcon
+                  className={`w-4 duration-300  ${
+                    openFilters[i] ? "rotate-90" : ""
+                  }`}
+                />
+              </button>
+              {openFilters[i] && (
+                <div className='text-sm py-4'>{filter.children}</div>
+              )}
+            </div>
+          );
+        })}
+      </div>
+      <div className='min-h-screen w-full bg-blue-400 md:col-span-4 '>
+        <div className='bg-fuchsia-400 px-4'>some header</div>
+        <div className='grid grid-cols-[repeat(auto-fit_,_minmax(250px_,_1fr))] gap-2 px-4 pb-4'>
+          <div className='bg-black min-h-[200px]'></div>
+          <div className='bg-black min-h-[200px]'></div>
+          <div className='bg-black min-h-[200px]'></div>
+          <div className='bg-black min-h-[200px]'></div>
+          <div className='bg-black min-h-[200px]'></div>
+          <div className='bg-black min-h-[200px]'></div>
+          <div className='bg-black min-h-[200px]'></div>
+          <div className='bg-black min-h-[200px]'></div>
+          <div className='bg-black min-h-[200px]'></div>
+          <div className='bg-black min-h-[200px]'></div>
+          <div className='bg-black min-h-[200px]'></div>
+          <div className='bg-black min-h-[200px]'></div>
+          <div className='bg-black min-h-[200px]'></div>
+          <div className='bg-black min-h-[200px]'></div>
+          <div className='bg-black min-h-[200px]'></div>
+          <div className='bg-black min-h-[200px]'></div>
+          <div className='bg-black min-h-[200px]'></div>
+          <div className='bg-black min-h-[200px]'></div>
+          <div className='bg-black min-h-[200px]'></div>
+          <div className='bg-black min-h-[200px]'></div>
+        </div>
+      </div>
+    </main>
+  );
 };
 
 export default Products;
